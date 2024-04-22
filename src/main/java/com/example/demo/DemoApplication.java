50f6fbc3e49582e4bad2f4100f0a43eef4226b70
sridharkarri18 <sridharkarri18@gmail.com>
2024-04-22 18:54:05 +0530

    Add  msg

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -8,6 +7,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("hai helloworld");
+		System.out.println("welcome");
 
 	}
 
