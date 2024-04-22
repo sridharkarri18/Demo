d298b65c4013da14367bc5470cf40961071b6c34
sridharkarri18 <sridharkarri18@gmail.com>
2024-04-22 18:29:01 +0530

    Added number

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -9,6 +8,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		System.out.println("hai helloworld");
 		System.out.println("hai mmmmmmmm");
+		System.out.println("9959700584");
 
 	}
 
