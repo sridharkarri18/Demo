ac3b4c05874b52ecf00026ab7dc5a81d71642382
sridharkarri18 <60732474+sridharkarri18@users.noreply.github.com>
2024-04-22 18:28:06 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("hai helloworld");
-		System.out.println("hai mmmmmmmm");
+		System.out.println("welcome");
 
 	}
 
