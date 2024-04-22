d58277d5afdd7190993ca811a331820fa6defa74
sridharkarri18 <sridharkarri18@gmail.com>
2024-04-22 18:45:28 +0530

    Removed and msg number

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,8 +8,6 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("hai helloworld");
-		System.out.println("welcome");
-		System.out.println("9959700584");
 
 	}
 
